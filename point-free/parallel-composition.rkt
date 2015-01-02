a1e121d784b9debc6d2911c6008793692a78e2f2
JackFirth <jackhfirth@gmail.com>
2015-01-02 00:02:10 -0800

    Composition tools
    Restructure for readability

--- point-free/parallel-composition.rkt
+++ point-free/parallel-composition.rkt
@@ -1,46 +1,40 @@
 #lang racket
 
-(provide join
-         join*
-         wind-pre
-         wind-post
-         wind
-         define/wind
-         define/wind-pre
-         define/wind-post
-         wind-pre*
-         wind-post*
-         wind*
-         define/wind-pre*
-         define/wind-post*
-         define/wind*)
+(provide    join    join*
+            
+            wind-pre     wind-post     wind
+            wind-pre*    wind-post*    wind*
+            
+            define/wind     define/wind-pre     define/wind-post
+            define/wind*    define/wind-pre*    define/wind-post*)
 
-(define (app f . vs) (apply f vs))
-(define ((join . fs) . vs) (apply values (map app fs vs)))
-(define ((join* f) . vs) (apply values (map f vs)))
+;; Helper macro for defining lots of simple macros
+(define-syntax-rule (define-syntaxes-rule [pattern expansion] ...)
+  (begin (define-syntax-rule pattern expansion) ...))
 
-(define (wind-pre f . gs) (compose f (apply join gs)))
-(define (wind-post f . gs) (compose (apply join gs) f))
-(define ((wind f . gs) . hs) (apply wind-post (apply wind-pre f gs) hs))
+;; Function form of flat application - (app + 2 3) is equivalent to (+ 2 3).
+;; Used to simplify logic in joining functins
+(define (app f . vs)           (apply f vs))
 
-(define-syntax-rule (define/wind id f (pre ...) (post ...))
-  (define id ((wind f pre ...) post ...)))
+;; Joining functions - composes functions in parallel
+(define ((join . fs) . vs)     (apply values (map app fs vs)))
+(define ((join* f) . vs)       (apply values (map f vs)))
 
-(define-syntax-rule (define/wind-pre id f pre ...)
-  (define id (wind-pre f pre ...)))
+;; Winding functions - composes functions in series and parallel
 
-(define-syntax-rule (define/wind-post id f post ...)
-  (define id (wind-post f post ...)))
+(define (wind-pre f . gs)      (compose f (apply join gs)))
+(define (wind-post f . gs)     (compose (apply join gs) f))
+(define ((wind f . gs) . hs)   (apply wind-post (apply wind-pre f gs) hs))
 
-(define (wind-pre* f g) (compose f (join* g)))
-(define (wind-post* f g) (compose (join* g) f))
-(define (wind* f g h) (wind-post* (wind-pre* f g) h))
+(define (wind-pre* f g)        (compose f (join* g)))
+(define (wind-post* f g)       (compose (join* g) f))
+(define (wind* f g h)          (wind-post* (wind-pre* f g) h))
 
-(define-syntax-rule (define/wind* id f pre post)
-  (define id (wind* f pre post)))
-
-(define-syntax-rule (define/wind-pre* id f pre)
-  (define id (wind-pre* f pre)))
-
-(define-syntax-rule (define/wind-post* id f post)
-  (define id (wind-post* f post)))
+;; Definition forms
+(define-syntaxes-rule
+  [(define/wind id f (pre ...) (post ...))    (define id ((wind f pre ...) post ...))]
+  [(define/wind-pre id f pre ...)             (define id (wind-pre f pre ...))]
+  [(define/wind-post id f post ...)           (define id (wind-post f post ...))]
+  [(define/wind* id f pre post)               (define id (wind* f pre post))]
+  [(define/wind-pre* id f pre)                (define id (wind-pre* f pre))]
+  [(define/wind-post* id f post)              (define id (wind-post* f post))])
