f79afdbaa20e435cd058d18b54242a50b8c242ff
JackFirth <jackhfirth@gmail.com>
2015-01-02 06:08:12 -0800

    Parallel composition
    Trimmed out definition forms to separate module

--- point-free/parallel-composition.rkt
+++ point-free/parallel-composition.rkt
@@ -1,22 +1,21 @@
 #lang racket
 
-(provide    join    join*
-            
-            wind-pre     wind-post     wind
-            wind-pre*    wind-post*    wind*
-            
-            define/wind     define/wind-pre     define/wind-post
-            define/wind*    define/wind-pre*    define/wind-post*)
-
-;; Helper macro for defining lots of simple macros
-(define-syntax-rule (define-syntaxes-rule [pattern expansion] ...)
-  (begin (define-syntax-rule pattern expansion) ...))
+(provide join
+         join*
+         wind-pre
+         wind-post
+         wind
+         wind-pre*
+         wind-post*
+         wind*)
 
 ;; Function form of flat application - (app + 2 3) is equivalent to (+ 2 3).
 ;; Used to simplify logic in joining functins
+
 (define (app f . vs)           (apply f vs))
 
 ;; Joining functions - composes functions in parallel
+
 (define ((join . fs) . vs)     (apply values (map app fs vs)))
 (define ((join* f) . vs)       (apply values (map f vs)))
 
@@ -29,12 +28,3 @@
 (define (wind-pre* f g)        (compose f (join* g)))
 (define (wind-post* f g)       (compose (join* g) f))
 (define (wind* f g h)          (wind-post* (wind-pre* f g) h))
-
-;; Definition forms
-(define-syntaxes-rule
-  [(define/wind id f (pre ...) (post ...))    (define id ((wind f pre ...) post ...))]
-  [(define/wind-pre id f pre ...)             (define id (wind-pre f pre ...))]
-  [(define/wind-post id f post ...)           (define id (wind-post f post ...))]
-  [(define/wind* id f pre post)               (define id (wind* f pre post))]
-  [(define/wind-pre* id f pre)                (define id (wind-pre* f pre))]
-  [(define/wind-post* id f post)              (define id (wind-post* f post))])
