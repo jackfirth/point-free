ba7c09450591b88ebd04d66bd4ef6a9820c5b608
Jack Firth <jackhfirth@gmail.com>
2015-01-12 16:29:08 -0800

    Update main.rkt
    Fix exports - definition forms now properly exported

--- point-free/main.rkt
+++ point-free/main.rkt
@@ -2,8 +2,10 @@
 
 (require "arg-count-syntax.rkt"
          "parallel-composition.rkt"
-         "thrush.rkt")
+         "thrush.rkt"
+         "definition-forms.rkt")
 
 (provide (all-from-out "arg-count-syntax.rkt"
                           "parallel-composition.rkt"
-                          "thrush.rkt"))
+                          "thrush.rkt"
+                          "definition-forms.rkt"))
