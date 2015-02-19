d4439f6fe54ab47ce3553bc12871870ba9f418d4
JackFirth <jackhfirth@gmail.com>
2015-02-18 23:50:54 -0800

    Add thrush tests

--- point-free/thrush.rkt
+++ point-free/thrush.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+
+(require rackunit)
+
 (provide thrush
          thrush+
          thrush*
@@ -11,11 +14,23 @@
 (define (thrush . fs)
   (apply compose (reverse fs)))
 
+(module+ test
+  (define symbol-length-even? (thrush symbol->string string-length even?))
+  (check-true (symbol-length-even? 'blah))
+  (check-false (symbol-length-even? 'foo)))
+
 (define (thrush+ v . fs)
   ((apply thrush fs) v))
 
+(module+ test
+  (check-true (thrush+ 'blah symbol->string string-length even?))
+  (check-false (thrush+ 'foo symbol->string string-length even?)))
+
 (define ((thrush* . vs) . fs)
   (apply (apply thrush fs) vs))
+
+(module+ test
+  (check string=? ((thrush* 2 3 4) * - number->string) "-24"))
 
 (define-syntax-rule (define/thrush id expr ...)
   (define id (thrush expr ...)))
