c077ef73fa26f1bd98a89413eb0762c7ad3037d8
JackFirth <jackhfirth@gmail.com>
2015-02-21 01:10:06 -0800

    Add build dep for doc-coverage

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define collection 'multi)
 (define deps '("base" "rackunit-lib"))
 (define build-deps '("cover"
+                     "doc-coverage"
                      "scribble-lib"
                      "rackunit-lib"
                      "racket-doc"))
